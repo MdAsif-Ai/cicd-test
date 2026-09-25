5192c0fc4fa825518303b7a1b1325c569dae991d
MdAsif-Ai <md.asifmd46@gmail.com>
2026-09-25 11:18:12 +0530

    initial commit

---
+++ test_factorial.py
@@ -0,0 +1,15 @@
+from math import factorial
+
+
+def test_factorial_zero():
+
+    assert factorial(0) == 1
+
+def test_factorial_one():
+    assert factorial(1) == 1
+
+def test_factorial_five():
+    assert factorial(5) == 120
+
+def test_factorial_ten():
+    assert factorial(10) == 3628800
